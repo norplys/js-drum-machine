7076e3fca6a9338dbe4b9e8fd8e89f9b3389a7ed
norplys <joywinata88@gmail.com>
2023-06-24 23:21:16 +0700

    updating config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/js-drum-machine",
+  base: "/js-drum-machine/",
   plugins: [react()],
 })
